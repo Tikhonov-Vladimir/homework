af9f0569b223dacd157771690e06853673f52a06
Tikhonov-Vladimir <v.s.tih27@gmail.com>
2020-04-06 17:49:46 +0300

    init

---
+++ task1.1/task1.1/task1.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	if (n < pow(10, 5)) {
+		vector <int> a(n);
+		for (int i = 0; i < n; i++)
+			cin >> a[i];
+		sort(a.begin(), a.end());
+		reverse(a.begin(), a.end());
+		for (auto value : a)
+			cout << value << " ";
+		return 0;
+	}
+	else
+		cout << "error";
+}
