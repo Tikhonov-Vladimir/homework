5623c4d91667cb2cc3ffd0701ddce0351ca30260
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2020-04-13 11:58:56 +0300

    Add files via upload

---
+++ horse/horse.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+int solve(int x1, int y1)
+{
+	if (x1 == 1 && y1 == 1)
+	{
+		return 1;
+	}
+	if (x1 < 0 || y1 < 0)
+	{
+		return 0;
+	}
+	return (solve(x1 - 1, y1 - 2) + solve(x1 - 2, y1 - 1));
+}
+int main()
+{
+	int n, m;
+	cin >> n >> m;
+	cout << solve(n, m);
+}
