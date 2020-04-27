7096f435c4a8fc266b08591d72c23d6df1632ec1
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2020-04-27 12:58:02 +0300

    Add files via upload

---
+++ test/exchange.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, s;
+    cin >> n >> s;
+    int j = n - 1;
+    int res = 0;
+    vector<int> mas(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> mas[i];
+    }
+    sort(mas.begin(), mas.end());
+    while (s > 0)
+    {
+        if (s >= mas[j])
+        {
+            res++;
+            s = s - mas[j];
+        }
+        else
+            j--;
+    }
+    cout << res << endl;
+    system("pause");
+    return 0;
+}
