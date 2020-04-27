b6f27905233de670fc3bf283c817f3ba2392ead2
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2020-04-27 12:51:24 +0300

    Add files via upload

---
+++ test/tems.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int n = 0;
+int counter = 1;
+vector<int> t;
+
+void out(int range) {
+	for (int i = 0; i < range; i++) {
+		cout << t[i] << '+';
+	}
+	cout << '\b' << endl;
+};
+
+void rec(int index, int sum, int last) {
+	if (sum == n) {
+		if (counter == 13672) out(index);
+		counter++;
+		return;
+	}
+	for (int i = last; i <= n - sum; i++)
+	{
+		t[index] = i;
+		rec(index + 1, sum + i, i);
+	}
+};
+
+int main() {
+	cin >> n;
+	terms = vector<int>(n);
+	rec(0, 0, 1);
+}
