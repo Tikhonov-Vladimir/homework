a8a2d2782486529d9ea57ba36acb1c7101bc94e9
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2020-06-05 12:23:44 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int str(const char* t, const char* p) { 
+    int tsize{}, psize{};
+
+    for (int i = 0; t[i] != '\0'; ++i)
+        ++tsize;
+
+    for (int i = 0; p[i] != '\0'; ++i)
+        ++psize;
+
+    if (psize <= tsize) {
+        int cur{}; 
+        for (int i = 0; i < tsize; ++i) {
+            if (t[i] != p[i - cur])
+                cur = i + 1;
+            if (i - cur + 1 == psize)
+                return cur;
+        }
+        return -1;
+    }
+    else
+        return -1;
+}
