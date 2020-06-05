2c01ffed31c8609cbb24b024b83930896ed2c720
Tikhonov-Vladimir <63228981+Tikhonov-Vladimir@users.noreply.github.com>
2020-06-05 12:53:51 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+﻿import java.util.ArrayDeque as Stack
+
+class SetOfStacks<T>(private val x : Int, 
+	private val y : Int) { 
+
+	private val stacks = MutableList(x) { Stack<T>() }
+
+	fun push(element: T) {
+		if (stacks.first().size == y) {
+			stacks.add(0, Stack())
+		}
+		stacks.first().push(element)
+	}
+
+	fun pop() {
+		stacks.first().pop()
+			stacks.removeIf{ it.isEmpty() }
+	}
+
+	fun first() = stacks.first().first
+}
